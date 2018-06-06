49c5c788362cc09ad8f27076d4718625c314b64c
jgomezp75 <jgomez@me.com>
2018-06-06 11:45:16 +0200

    Delete clavesCSOD_pro.R

--- clavesCSOD_pro.R
+++
@@ -1,6 +0,0 @@
-apiKey <- "ulb3f4micb70"
-apiSecret <-
-    "v8/Nt1qrcWV6ScCqg0do9izYFpNh6/iHA3xi/No5V4+dPMz7Mz/vxhLeU78JOy9XEXx1eoFLxngFYvCXuAB8Ew=="
-user <- "adm_rgarcia"
-
-entorno <- "https://bbva.csod.com"
