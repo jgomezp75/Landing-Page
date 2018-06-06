2d8cd41a1f402554562b96ea21bdae28d5b1b184
jgomezp75 <jgomez@me.com>
2018-06-06 18:23:36 +0200

    commit

--- obtenerDatosBase.R
+++ obtenerDatosBase.R
@@ -10,7 +10,20 @@ source("clavesCSOD_pro.R")
                   sesion$sessionToken,
                   sesion$sessionSecretKey,
                   "")
+    vw_rpt_requisition <-
+        leerOdata("vw_rpt_requisition",
+                  sesion$sessionToken,
+                  sesion$sessionSecretKey,
+                  "")
+    vw_rpt_requisition_posting <-
+        leerOdata("vw_rpt_requisition_posting",
+                  sesion$sessionToken,
+                  sesion$sessionSecretKey,
+                  "")
     parte_B <- vw_rpt_recruiting %>% filter(ats_req_status=="Open",
-                                            ats_req_posting_career_center=="Yes")
+                                            ats_req_posting_career_center=="Yes"
+                                            ) %>% 
+        left_join(vw_rpt_requisition_posting,by = c("ats_req_id" = "posting_job_requisition_id")) 
+        
         
 #}
